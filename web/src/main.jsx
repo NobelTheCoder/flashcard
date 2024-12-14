636a417940ffc531675809686736109bb878a86f
Nobel Tadee <nobeltadele.official@gmail.com>
2024-12-14 19:26:18 +0300

    Added navigation url

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import GET from './get.jsx'
-import App from "./App.jsx"
+import GET from './get.jsx';
+import App from './App.jsx';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GET />
+    <Router>
+      <Routes>
+        <Route path="/" element={<GET />} />
+        <Route path="/push" element={<App />} />
+      </Routes>
+    </Router>
   </StrictMode>,
-)
+);
